3214cadb1384e81151986d34de1a2c65d202a7b8
Rodrigo <geekrodrigo@live.com>
2019-01-15 17:19:12 -0300

    Commit basic setup.sh and install.sh for my personal openbox settings/themes

---
+++ install.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/rodlessa/openbox.git && echo Inciando copy &&cp openbox/* ~/
+
---
+++ setup.sh
@@ -0,0 +1,1 @@
+sudo apt-get install wget git tint2 xcompmgr startx nitrogen volumeicon dusnt thunar lxappearance && wget install.sh && chmod -x install.sh && ./install.sh
